85bb2e1dbb391a81d8708634a016cf73668f63c7
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-06-05 11:14:31 +0100

    Find highest spread and return corresponding days in array

--- script.rb
+++ script.rb
@@ -1,10 +1,7 @@
 def stock_picker (stock_price_array)
-  best_day_to_buy = nil
-  best_day_to_sell = nil
   positive_spreads_and_corresponding_days = []
 
-
-  # calculate spreads and save into array if positive (positive_spreads_and_corresponding_days = [[spread, day_to_buy, day_to_sell]])
+  # calculate spreads and save into array if positive (positive_spreads_and_corresponding_days = [[profit, day_to_buy, day_to_sell]])
   stock_price_array.each_with_index do |price_to_buy, day_to_buy| 
     stock_price_array.each_with_index do |price_to_sell, day_to_sell|
       if day_to_buy <= day_to_sell && price_to_sell > price_to_buy
@@ -13,6 +10,19 @@ def stock_picker (stock_price_array)
       end
     end
   end
+
+  #find highest spread and save corresponding days in array
+  highest_spread = 0
+  best_days = [nil, nil]
+
+  positive_spreads_and_corresponding_days.each do |spread_buy_sell|
+    if spread_buy_sell[0] > highest_spread
+      highest_spread = spread_buy_sell[0]
+      best_days[0] = spread_buy_sell[1]
+      best_days[1] = spread_buy_sell[2]
+    end
+  end
+  best_days
 end
 
 p stock_picker([17,3,6,9,15,8,6,1,10])
